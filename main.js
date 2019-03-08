dba80ea9c78cde52a2eac0f17d9d78bac2bb5470
sangphan005 <38428098+sangphan005@users.noreply.github.com>
2019-03-08 10:30:37 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const socket = io('http://localhost:3000');
-
+//const socket = io('http://localhost:3000');
+  const socket = io('https://sangphan005.github.io/');
 $('#div-chat').hide();
 
 socket.on('DANH_SACH_ONLINE', arrUserInfo => {
